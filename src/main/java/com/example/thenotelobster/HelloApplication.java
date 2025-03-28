efd9ee11d3d7496fdbac0d0813478976b777f8a7
Hudson <hudson.aberle@hotmail.com>
2025-03-28 11:37:40 +1000

    added basic interface for log in screen

--- src/main/java/com/example/thenotelobster/HelloApplication.java
+++ src/main/java/com/example/thenotelobster/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
