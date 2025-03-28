4176a06c7a69d8c577aa4021f88ccea2dbb16ae7
Hudson <hudson.aberle@hotmail.com>
2025-03-28 12:50:12 +1000

    added create account logic

---
+++ src/main/java/com/example/thenotelobster/CreateAccController.java
@@ -0,0 +1,25 @@
+package com.example.thenotelobster;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreateAccController {
+
+    @FXML
+    private Button contuineButton;
+
+    @FXML
+    protected void onContuineButtonClick() throws IOException {
+        Stage stage = (Stage) contuineButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+        scene.getStylesheets().add(stylesheet);
+        stage.setScene(scene);
+    }
+}
