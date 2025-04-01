a1c1d7295dc0ecea9b7eaff72d02075cdd10ac32
Harrison <harrison.warner@connect.qut.edu.au>
2025-04-01 12:31:38 +1000

    Added summary-view and SummaryController

--- src/main/java/com/example/thenotelobster/MainController.java
+++ src/main/java/com/example/thenotelobster/MainController.java
@@ -7,15 +7,32 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainController {
 
-@FXML private TextArea AddNotes;
+    @FXML private TextArea AddNotes;
 
-@FXML private TextArea SourceLinks;
+    @FXML private TextArea SourceLinks;
 
-@FXML private Button SummarizeNotes;
+    @FXML private Button SummarizeButton;
 
-@FXML private TextField NotesSubject;
+    @FXML private Button NotesButton;
 
+    @FXML private TextField NotesSubject;
 
+    @FXML
+    protected void onSummarizeClick() throws IOException {
+        Stage stage = (Stage) SummarizeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("summary-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+        scene.getStylesheets().add(stylesheet);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    protected void onNotesClick() {
+        // Implement connection to notes page/ view
+    }
 }
