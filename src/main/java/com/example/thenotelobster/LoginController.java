824a26e2d5a050187bc5a56a09aa3a536e3f246d
Hudson <hudson.aberle@hotmail.com>
2025-04-04 12:17:55 +1000

    added login logic

--- src/main/java/com/example/thenotelobster/LoginController.java
+++ src/main/java/com/example/thenotelobster/LoginController.java
@@ -17,6 +17,12 @@ public class LoginController {
     private Button loginButton;
 
     @FXML
+    private TextField emailText;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
     protected void onCreateAccButtonClick() throws IOException {
         Stage stage = (Stage) createAccButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create-acc-view.fxml"));
@@ -28,11 +34,17 @@ public class LoginController {
 
     @FXML
     protected void onLoginButtonClick() throws IOException {
-        Stage stage = (Stage) loginButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
-        scene.getStylesheets().add(stylesheet);
-        stage.setScene(scene);
+        UserAccountDAO userAccountDAO = new UserAccountDAO();
+        UserAccount account = userAccountDAO.getByEmail(emailText.getText());
+
+        if (account.getPassword() == passwordText.getText()); {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+            String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+            scene.getStylesheets().add(stylesheet);
+            stage.setScene(scene);
+        }
+
     }
 }
