8afa8e2067ff2533a7e9a7391a06e8e42efa32ca
Hudson <hudson.aberle@hotmail.com>
2025-04-04 11:36:28 +1000

    added basic login logic wihtout checking details

--- src/main/java/com/example/thenotelobster/LoginController.java
+++ src/main/java/com/example/thenotelobster/LoginController.java
@@ -14,9 +14,22 @@ public class LoginController {
     private Button createAccButton;
 
     @FXML
+    private Button loginButton;
+
+    @FXML
     protected void onCreateAccButtonClick() throws IOException {
         Stage stage = (Stage) createAccButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("create-acc-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
+        scene.getStylesheets().add(stylesheet);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    protected void onLoginButtonClick() throws IOException {
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
         String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
